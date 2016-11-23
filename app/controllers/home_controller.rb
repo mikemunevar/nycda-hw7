31066b65d57dfc31066ff92ae1c6157601e279b3
mikemunevar <mikemunevar@gmail.com>
2016-11-23 01:33:03 +0000

    Updated Home Controller helper methods.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,19 @@ class HomeController < ApplicationController
     def index
     end
 
+  helper_method :resource_name, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+
 end
