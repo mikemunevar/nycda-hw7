f7ca5b712bf31b24d5af6770af8be76f4ca10ff0
mikemunevar <mikemunevar@gmail.com>
2016-11-29 21:02:04 +0000

    Added the edit user page, with confirmation.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+require 'pp'
+
+class UsersController < ApplicationController
+    
+  public
+  
+  before_action(:get_user, only: [:show, :edit, :update, :destroy])
+
+  def index
+    # Create a "collection" of users.  A collection is a special rails datatype.  You can use all functions from array on the collection. #MDM
+    @users = User.all
+  end
+
+  # Create a "new" controller so that you can use a form helper #MDM
+  def new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # puts @user   
+    # render json: @user
+  end
+
+  def edit
+    # @user = User.find(params[:id])
+    # get_user
+  end
+
+  def create
+    @user = User.create(user_params)
+    # redirect_to(@user)
+    redirect_to_user("You just created a new user.")
+  end
+
+
+  def update
+    # @user = User.find(params[:id])
+    # get_user
+    # @user.update(params[:user].permit(:title, :body)) # These are the only fields that we have on the form, so that's the only ones we will let through
+    @user.update(user_params)
+    # redirect_to(@user)
+    redirect_to_user("You just updated the user")
+  end
+
+  # The private method does not let this be called through the URL. #MDM
+  private  # Refactor here:
+  def user_params
+    params[:user].permit(:name, :headline, :profile, :email)  
+  end  
+  
+  def get_user
+    @user = User.find(params[:id])
+  end
+  
+  def redirect_to_user(notice)
+    redirect_to(@user, notice: notice )
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  # The below is advised, but it doens't change anything in the routes! #MDM 
+  # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
   root to: "home#index"
+  
+  resources(:users)
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
