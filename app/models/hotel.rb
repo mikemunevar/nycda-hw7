fe4b6b9ce7005c2528530a493d21e36439ff6c7b
mikemunevar <mikemunevar@gmail.com>
2016-12-02 14:58:12 +0000

    Completed model validations and edit for only currently signed in users.

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,18 @@
 class Hotel < ActiveRecord::Base
+    
+    validates(:user_id,
+      presence: true,
+      length: {minimum: 5}
+      )
+    
+    validates(:name,
+      presence: true,
+      uniqueness: true
+      )  
+    
+    validates(:location,
+      presence: true
+      )
+      
+      
 end
